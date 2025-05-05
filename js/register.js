15a621e2b320d229e31691b9406a185319ce5fd3
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-05-06 01:28:28 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/+BgeWyRaZ8UkyOWUy';
+    window.location.href = 'https://t.me/+_zNMxMi7cHplYmMy';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
