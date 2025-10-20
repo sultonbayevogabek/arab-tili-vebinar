74615661f0f571e1dfa087382c1662d899e10f76
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-10-21 00:01:23 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -2,32 +2,18 @@
 
 (async _ => {
   let user = JSON.parse(localStorage.getItem('user'));
-
-  const joinButton = document.querySelector('#joinToChannel')
-  joinButton?.addEventListener('click', async (e) => {
-    window.location.href = 'https://t.me/+EWhH9PtohlQ3OTk6';
-  })
-
   if (user && user?.name && user?.phone && user?.time) {
-    const utmParams = statistics.getUtmParams();
-    console.log(utmParams);
     const formData = new FormData();
 
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    formData.append(`Foydalanuvchi ID`, statistics.userId);
-    formData.append(`Timestamp`, statistics.time?.toString());
-    formData.append('utm_source', utmParams.utmSource);
-    formData.append('utm_medium', utmParams.utmMedium);
-    formData.append('utm_campaign', utmParams.utmCampaign);
 
-    let response = await fetch('https://script.google.com/macros/s/AKfycby4N1_b_K6KcdqwGEme2qHUF6EqW6XXFtDcisiUWYKKwM3ylvOu73eAIsG-2yNrRvBvyA/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyutQTZdDwFBkKK8F3mZMPYyWoNRar_s-cr8pFIdsgMMmUh2PfJjPNKDU6pUqKPwtZS/exec', {
       method: 'POST',
       body: formData
     })
     await response.json();
-    await statistics.onRegister(user);
     localStorage.removeItem('user');
   } else {
     localStorage.removeItem('user');
